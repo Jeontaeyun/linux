15ad9ade04962ef5bf828de5829bf8429a240c4f
Jeontaeyun <jow1813@naver.com>
2020-06-16 22:09:36 +0900

    Feat : Implement grep command architecture

--- grep/grep.c
+++ grep/grep.c
@@ -1,1 +1,16 @@
+/**
+ * 01. Regular Expression(Regex) Support
+ **/
 #include <stdio.h>
+#include <sys/types.h>
+#include <regex.h>
+
+static void do_grep(regex_t *pat, FILE *src);
+
+int main(int argc, char *argv)
+{
+}
+
+static void do_grep(regex_t *pat, FILE *src)
+{
+}
