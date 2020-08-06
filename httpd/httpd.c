6002883fd2ab804d24b62d8cfa57ca4e5647aa53
Jeontaeyun <jow1813@naver.com>
2020-08-07 00:36:48 +0900

    Feat : implement validate_directory

--- httpd/httpd.c
+++ httpd/httpd.c
@@ -9,7 +9,9 @@
 #include <stdarg.h>
 #include <ctype.h>
 #include <signal.h>
+#include <sys/stat.h>
 
+#define _FILE_OFFSET_BITS 64
 #define SERVER_NAME "CONNECT_HTTP"
 #define SERVER_VERSION "1.0"
 #define HTTP_MINOR_VERSION 0
@@ -40,6 +42,16 @@ struct FileInfo
     int ok;
 };
 
+typedef void (*sighandler_t)(int);
+static void install_signal_handlers(void);
+static void service(FILE *in, FILE *out, char *docroot);
+static void log_exit(char *fmt, ...);
+static void *xmalloc(size_t sz);
+static void install_signal_handlers(void);
+static void trap_signal(int sig, sighandler_t handler);
+static void signal_exit(int sig);
+static void validate_directory(char *path);
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -47,6 +59,70 @@ int main(int argc, char *argv[])
         fprintf(stderr, "USAGE: %s <docroot>\n", argv[0]);
         exit(1);
     }
+    validate_directory(argv[1]);
+    exit(0);
+}
 
+// ... mean Variable Arguments need #include <stdarg.h>
+static void
+log_exit(char *fmt, ...)
+{
+    /**
+    * Usage of variable arguments
+    * vs_list ap;
+    * vs_start(ap, fmt)
+    * /-- use ap --/
+    * vs_end(ap)
+    **/
+    va_list ap;
+
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fputc('\n', stderr);
+    va_end(ap);
+    exit(1);
+}
+
+static void *
+xmalloc(size_t sz)
+{
+    void *p;
+
+    p = malloc(sz);
+    if (!p)
+        log_exit("failed to allocate memory");
+    return p;
+}
+
+static void
+install_signal_handlers(void)
+{
+    trap_signal(SIGPIPE, signal_exit);
+}
+
+static void trap_signal(int sig, sighandler_t handler)
+{
+    struct sigaction act;
+    act.sa_handler = handler;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_RESTART;
+    if (sigaction(sig, &act, NULL) < 0)
+        log_exit("sigaction() failed: %s", strerror(errno));
+}
+
+static void signal_exit(int sig)
+{
+    log_exit("exit by signal %d", sig);
+}
+
+static void validate_directory(char *path)
+{
+    struct stat *buf;
+    if (stat(path, buf) < -1)
+    {
+        log_exit("path is not directory");
+    }
+    if (S_ISDIR(buf->st_mode))
+        printf("DIRECTORY");
     exit(0);
 }
